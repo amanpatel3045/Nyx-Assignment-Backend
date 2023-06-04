d9284b2a17294c89916c5f0822543d7cf4277605
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-06-04 12:04:04 +0530

    done

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+  db: `mongodb+srv://amanpatel3045:amanpatel3045@cluster0.ynykyrz.mongodb.net/`
+};
